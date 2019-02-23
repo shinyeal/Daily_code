04afd831be39e9ec323d5c589f0365f943edf78e
shinyeal <1670806387@qq.com>
2019-02-23 17:40:04 +0800

    二叉树前中后序遍历

--- data_structure/6.binary_tree.cpp
+++ data_structure/6.binary_tree.cpp
@@ -18,64 +18,64 @@ typedef struct Node {
 
 Node *init(int data) {
     Node *p = (Node *)malloc(sizeof(Node));
-    p -> data = data;
-    p -> lchild = p -> rchild = NULL;
+    p->data = data;
+    p->lchild = p->rchild = NULL;
     return p;
 }
 
 void clear(Node *node) {
     if(node == NULL) return ;
-    clear(node -> lchild);
-    clear(node -> rchild);
+    clear(node->lchild);
+    clear(node->rchild);
     free(node);
     return ;
 }
 
 Node *build() {
     Node *root = init(1);
-    root -> lchild = init(2);
-    root -> rchild = init(3);
-    root -> lchild -> lchild = init(6);
-    root -> lchild -> rchild = init(7);
-    root -> lchild -> lchild -> rchild = init(12);
-    root -> rchild -> lchild = init(5);
-    root -> rchild -> rchild = init(9);
-    root -> rchild -> lchild -> lchild = init(10);
-    root -> rchild -> lchild -> rchild = init(11);
+    root->lchild = init(2);
+    root->rchild = init(3);
+    root->lchild->lchild = init(6);
+    root->lchild->rchild = init(7);
+    root->lchild->lchild->rchild = init(12);
+    root->rchild->lchild = init(5);
+    root->rchild->rchild = init(9);
+    root->rchild->lchild->lchild = init(10);
+    root->rchild->lchild->rchild = init(11);
     return root;
 }
 
 int height_tree(Node *root) {
     if(root == NULL) return 0;
-    int lh = height_tree(root -> lchild);
-    int rh = height_tree(root -> rchild);
+    int lh = height_tree(root->lchild);
+    int rh = height_tree(root->rchild);
     return (lh > rh ? lh : rh) + 1;
 }
 
 //前序遍历
 void preorder(Node *root) {
     if(root == NULL) return ;
-    printf("%d ", root -> data);
-    preorder(root -> lchild);
-    preorder(root -> rchild);
+    printf("%d ", root->data);
+    preorder(root->lchild);
+    preorder(root->rchild);
     return ;
 }
 
 //中序遍历
 void inorder(Node *root) {
     if(root == NULL) return ;
-    inorder(root -> lchild);
-    printf("%d ", root -> data);
-    inorder(root -> rchild);
+    inorder(root->lchild);
+    printf("%d ", root->data);
+    inorder(root->rchild);
     return ;
 }
 
 //后序遍历
 void postorder(Node *root) {
     if(root == NULL) return ;
-    postorder(root -> lchild);
-    postorder(root -> rchild);
-    printf("%d ", root -> data);
+    postorder(root->lchild);
+    postorder(root->rchild);
+    printf("%d ", root->data);
     return ;
 }
 
